d6d0694ad85cc0f2e545f3ae1070094b5fe71fa0
Green Man <brendan.mccaughey@gmail.com>
2018-10-14 23:13:57 -0400

    HTML and Bootstrap set up. BEgun planning JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Planning
+//Global Variables
+//=================
+//Set up an array of trivia questions to write to the HTML or...
+//Put each question in the HTML and hide and show as each question is answered
+$(window).ready (function () {
+    $('#pageLoad').modal ('show')
+});
+$(document).ready(function () {
+
+
+
+
+});
