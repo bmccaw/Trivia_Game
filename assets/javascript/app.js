e1a5b2b7cde0e3b3978578dc8c235c36df0067c3
Green Man <brendan.mccaughey@gmail.com>
2018-10-19 21:17:19 -0400

    Additional styling. Added background music.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,73 +16,73 @@ $(document).ready(function () {
             question: 'Which of these dwarves is the father of Gimli?',
             choices: ['Gloin', 'Oin', 'Dwalin', 'Balin'],
             correctAnswer: 0,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/kyevhryWKHk?controls=0&amp;start=56&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/kyevhryWKHk?controls=0&amp;start=56&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
-            question: 'What is the name of the massive statues the fellowship encountered as they traveled down the river Anduin?',
+            question: 'What is the name of the massive stone monuments the fellowship encountered as they traveled down the river Anduin?',
             choices: ['Amon Hen', 'Amon Sul', 'Argonath', 'Dol Amroth'],
             correctAnswer: 2,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/SlLC1kCH1ps?controls=0&amp;start=38&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/SlLC1kCH1ps?controls=0&amp;start=38&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'This area of the Kingdom of Gondor is where Frodo met the ranger Faramir.',
             choices: ['Lothlorien', 'Cair Andros', 'Arnor', 'Ithilien'],
             correctAnswer: 3,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/ar_aKXzr-Z4?controls=0&amp;start=106&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/ar_aKXzr-Z4?controls=0&amp;start=106&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'Which of these names does NOT refer to Aragorn?',
             choices: ['Strider', 'Elessar', 'Anarion', 'Thorongil'],
             correctAnswer: 2,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/r1j9LVVUiFg?controls=0&amp;start=231&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/BtEC3pNEMhY?controls=0&amp;start=24&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
-            question: 'Which of these is NOT one of the five wizards?',
-            choices: ['Gandalf', 'Celebrimbor', 'Radagast', 'Saruman'],
+            question: 'Which of these characters was NOT a member of the Fellowship?',
+            choices: ['Legolas', 'Bilbo', 'Boromir', 'Merry'],
             correctAnswer: 1,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/xaThfLzuyjE?controls=0&amp;start=0&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/CvAXidAPTRM?controls=0&amp;start=25&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'Who is "The Necromancer" that occupies Dol Guldur?',
             choices: ['Sauron', 'Melkor', 'Saruman', 'Khamul'],
             correctAnswer: 0,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/PIrVmJeI1_8?controls=0&amp;start=110&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/PIrVmJeI1_8?controls=0&amp;start=110&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'This creature of fire and shadow was known to the dwarves as "Durin\'s Bane."',
             choices: ['Shelob', 'Uruk-hai', 'Nazgul', 'Balrog'],
             correctAnswer: 3,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/mJZZNHekEQw?controls=0&amp;start=6&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/mJZZNHekEQw?controls=0&amp;start=6&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'Which of these characters did NOT possess a ring of power?',
             choices: ['Galadriel', 'Aragorn', 'Elrond', 'Gandalf'],
             correctAnswer: 1,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/K3VOf3CBGvw?controls=0&amp;start=261&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/K3VOf3CBGvw?controls=0&amp;start=261&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'At the beginning of "The Fellowship of the Ring", what birthday is Bilbo celebrating"?',
             choices: ['100', '111', '99', '101'],
             correctAnswer: 1,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/s2sVMLr1fDg?controls=0&amp;start=17&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/s2sVMLr1fDg?controls=0&amp;start=17&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'In the book, "The Fellowship of the Ring," what elf meets Aragorn and the hobbits on the way to Rivendell?',
             choices: ['Arwen', 'Elrond', 'Haldir', 'Glorfindel'],
             correctAnswer: 3,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/N4I7myAG_mY?controls=0&amp;start=144&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/N4I7myAG_mY?controls=0&amp;start=144&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
-            question: 'This sword, also known as "Flame of the West," was reforged from the shards of Narsil',
+            question: 'This sword, also known as "Flame of the West," was reforged from the shards of Narsil.',
             choices: ['Anduril', 'Glamdring', 'Orcrist', 'Gurthang'],
             correctAnswer: 0,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/fsbDigj7w5c?controls=0&amp;start=202&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/fsbDigj7w5c?controls=0&amp;start=202&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
         {
             question: 'In "The Fellowship of the Ring," who is Sam in relation to Frodo?',
             choices: ['Brother', 'Cousin', 'Gardener', 'Housekeeper'],
             correctAnswer: 2,
-            videoCode: '<iframe width="800" height="360" src="https://www.youtube.com/embed/JXuqJ4c1dxE?controls=0&amp;start=68&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
+            videoCode: '<iframe width="750" height="360" src="https://www.youtube.com/embed/JXuqJ4c1dxE?controls=0&amp;start=68&amp;modestbranding=1&autoplay=1&disablekb&rel=0&showinfo=0&iv_load_policy=3" </iframe>'
         },
     ];
     let userGuess = '';
@@ -100,24 +100,16 @@ $(document).ready(function () {
     let timeRemaining = 30;
     var running = false;
     let triviaTime;
-
-    //RESET function once game is complete
-    // function reset() {
-
-    //     index = 0;
-    //     let correct = 0;
-    //     let incorrect = 0;
-    //     let unanswered = 0;
-    //     $('#answerarea').empty();
-    // }
  
     $('#reset').hide();    
-    
+    $('body').click(function() {
+        $('audio').get(0).play();
+        $('#bgmusic').prop('volume', 0.5);
+    });
 
     $('#start').click(function () { //calls the first question and resets score to 0 and timer to 30 sec
         $(this).hide();
         $('#ring').hide();
-        // reset();
         timer();
         getQuestion();
         for(var i = 0; i < questions.length; i++) {
@@ -133,6 +125,8 @@ $(document).ready(function () {
             userGuess ='';
             $('#answerarea').html('<h3>Correct!</h3>')
             displayResults();
+            document.getElementById("bgmusic").muted = true;
+
         }
         else {
             stop();
@@ -140,6 +134,8 @@ $(document).ready(function () {
             userGuess = '';
             $('#answerarea').html('<h3>Wrong! The correct answer is ' + pick.choices[pick.correctAnswer] + '</h3>');
             displayResults();
+            document.getElementById("bgmusic").muted = true;
+
 
         }
         console.log('Working!');
@@ -168,6 +164,7 @@ $(document).ready(function () {
             clearInterval(triviaTime)
             $('#answerarea').html('<h3>Out of Time! The correct answer is ' + pick.choices[pick.correctAnswer] + '</h3>');
             displayResults();
+            document.getElementById("bgmusic").muted = true;
             
             console.log(unanswered);
         }
@@ -222,14 +219,17 @@ $(document).ready(function () {
         }
         else {
             $('#answerarea').prepend('<h4>All is lost!</h4>')
+            document.getElementById("bgmusic").muted = false;
         }
         }
         else {
             timer(); 
             getQuestion();
+            document.getElementById("bgmusic").muted = false;
         }
         }, 20000);
         
+
         console.log('Question count: ' + questionCount);
     }
     //Play Again button with reset function on click
